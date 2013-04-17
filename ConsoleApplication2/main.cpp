d81f552812eaff064d395bd920c826dc330a3f4b
Eugene Pavlov <bjohn.ext@gmail.com>
2013-04-17 09:11:12 +0400

    add new project

---
+++ ConsoleApplication2/main.cpp
@@ -0,0 +1,73 @@
+// директивы подключения заголовочных файлов
+#include "stdio.h" // поиск файла в текущей директории
+#include <malloc.h> // поиск файла в системных директориях
+#include <string.h>
+
+
+class Record
+{
+public:
+    char* name;
+    int sex;
+    int age;
+
+	Record() {}
+	~Record() {if (NULL != name) free(name);}
+
+	void PrintRecord();
+	void FPrintRecord(FILE* pFile);
+	int FScanRecord(FILE* pFile);
+};
+
+int main ()
+{
+
+    Record item;
+
+    item.name = (char*)malloc((strlen("John")+1) * sizeof(char)); 
+	if (item.name == NULL) return -1;
+    strncpy(item.name, "John", strlen("John")+1);
+
+    item.sex = 1;
+    item.age = 33;
+
+    FILE* pFile;
+    pFile = fopen ("myfile.txt","a");
+    
+    if (pFile != NULL)
+    {
+        item.FPrintRecord(pFile);
+        fclose(pFile);
+    }
+
+    pFile = fopen ("myfile.txt","r");
+    if (pFile != NULL)
+    {
+        while (item.FScanRecord(pFile) != EOF)
+        {
+            item.PrintRecord();
+        }
+        fclose(pFile);
+    }
+
+    scanf("%s\n");
+	
+	free(item.name);
+
+    return 0;
+}
+
+void Record::PrintRecord()
+{
+	printf ("%d %d %s\n",age, sex, name);
+}
+
+void Record::FPrintRecord(FILE* pFile)
+{
+	fprintf (pFile, "%d %d %s\n",age, sex, name);
+}
+
+int Record::FScanRecord(FILE* pFile)
+{
+	return fscanf (pFile, "%d %d %s\n", age, sex, name);
+}
